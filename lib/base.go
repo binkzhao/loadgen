afdaca3ecf47cb8491f86402852a2a07d5cefd82
Bink <781270119@qq.com>
2018-08-21 10:44:55 +0800

    调度器实现

--- lib/base.go
+++ lib/base.go
@@ -2,6 +2,9 @@ package lib
 
 import "time"
 
+// RetCode 表示结果代码的类型。
+type RetCode int
+
 // 用于表示原生请求的结构
 type RawReq struct {
 	ID  uint64
@@ -12,12 +15,34 @@ type RawReq struct {
 type RawResp struct {
 	ID     uint64
 	Resp   []byte
-	err    error
+	Err    error
 	Elapse time.Duration
 }
 
+// GetRetCodePlain 会依据结果代码返回相应的文字解释。
+func GetRetCodePlain(code RetCode) string {
+	var codePlain string
+	switch code {
+	case RET_CODE_SUCCESS:
+		codePlain = "Success"
+	case RET_CODE_WARNING_CALL_TIMEOUT:
+		codePlain = "Call Timeout Warning"
+	case RET_CODE_ERROR_CALL:
+		codePlain = "Call Error"
+	case RET_CODE_ERROR_RESPONSE:
+		codePlain = "Response Error"
+	case RET_CODE_ERROR_CALEE:
+		codePlain = "Callee Error"
+	case RET_CODE_FATAL_CALL:
+		codePlain = "Call Fatal Error"
+	default:
+		codePlain = "Unknown result code"
+	}
+	return codePlain
+}
+
 type CallResult struct {
-	ID     int64         // ID
+	ID     uint64         // ID
 	Req    RawReq        // 原生请求
 	Resp   RawResp       // 原生相应
 	Code   RetCode       // 相应代码
--- lib/consts.go
+++ lib/consts.go
@@ -13,3 +13,13 @@ const (
 	// 代表已停止
 	STATUS_STOPPED = 4
 )
+
+// 保留 1 ~ 1000 给载荷承受方使用。
+const (
+	RET_CODE_SUCCESS              RetCode = 0    // 成功。
+	RET_CODE_WARNING_CALL_TIMEOUT         = 1001 // 调用超时警告。
+	RET_CODE_ERROR_CALL                   = 2001 // 调用错误。
+	RET_CODE_ERROR_RESPONSE               = 2002 // 响应内容错误。
+	RET_CODE_ERROR_CALEE                  = 2003 // 被调用方（被测软件）的内部错误。
+	RET_CODE_FATAL_CALL                   = 3001 // 调用过程中发生了致命错误！
+)
